d491109c958af660751a4a8c3227980a825e9dfc
michael dudley <michael.dudley@improvingenterprises.com>
2015-10-21 23:20:40 -0500

    edit logic

---
+++ Domain/People/Update/UpdatePerson.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Domain.Repository;
+using MediatR;
+
+namespace Domain.People.Update
+{
+    public class UpdatePerson : IAsyncRequest<Person>
+    {
+        public int Id { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+    }
+
+    public class UpdatePersonHandler : IAsyncRequestHandler<UpdatePerson, Person>
+    {
+        private readonly IRepository _repository;
+
+        public UpdatePersonHandler(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Person> Handle(UpdatePerson update)
+        {
+            var person = await  _repository.FindAsync(update.Id);
+            person.First = update.First;
+            person.Last = update.Last;
+
+            return await _repository.UpdateAsync(person);
+        }
+    }
+}
--- IntroToASPNetMVC/Controllers/PeopleController.cs
+++ IntroToASPNetMVC/Controllers/PeopleController.cs
@@ -3,6 +3,7 @@ using System.Web.Mvc;
 using Domain.People.Create;
 using Domain.People.Delete;
 using Domain.People.Get;
+using Domain.People.Update;
 using MediatR;
 
 namespace IntroToASPNetMVC.Controllers
@@ -38,6 +39,19 @@ namespace IntroToASPNetMVC.Controllers
         {
             var person = await _mediator.SendAsync(getPerson);
             return View(person);
+        }
+
+        public async Task<ActionResult> Edit(GetPerson getPerson)
+        {
+            var person = await _mediator.SendAsync(getPerson);
+            return View(person);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(UpdatePerson getPerson)
+        {
+            var person = await _mediator.SendAsync(getPerson);
+            return RedirectToAction("Details", new { Id = person.Id });
         }
 
         public async Task<ActionResult> Delete(GetPerson getPerson)
